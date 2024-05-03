c863908a68d7b0320d9ffd537083b3afc282bcbd
ghandidev <ghandiaboultaif@gmail.com>
2024-05-03 12:28:53 -0400

    adding functions getMean and getMedian

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
 const getMean = (array) =>
   array.reduce((acc, el) => acc + el, 0) / array.length;
+
+const getMedian = (array) => {
+  const sorted = array.sort((a, b) => a - b);
+  const median =
+    array.length % 2 === 0
+      ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]])
+      : sorted[Math.floor(array.length / 2)];
+  return median;
+
+  /* const length = sorted.length;
+
+  if (length % 2 === 0) {
+    const median = length / 2;
+    const median1 = sorted[median - 1];
+    const median2 = sorted[median];
+    return getMean([median1, median2]);
+  } else {
+    const median = Math.floor(length / 2);
+    return sorted[median];
+  } */
+};
+
+const getMode = (array) => {};
+
 const calculate = () => {
   const value = document.querySelector('#numbers').value;
   const array = value.split(/,\s*/g);
   const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
-  const mean = getMean(numbers);
 
+  const mean = getMean(numbers);
+  const median = getMedian(numbers);
   document.querySelector('#mean').textContent = mean;
+  document.querySelector('#median').textContent = median;
 };
+
+/* const testArr1 = [1, 2, 3, 4, 5];
+const testArr2 = [1, 2, 3, 4, 5, 6];
+const isEven = testArr2.length % 2 === 0;
+console.log(isEven);
+const oddListMedian = testArr1[Math.floor(testArr1.length / 2)];
+console.log(oddListMedian);
+const evenListMedian = getMean(testArr2);
+console.log(evenListMedian); */
