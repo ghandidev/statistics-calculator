b1d455788546456c9fa8e72dd1dddec49c660974
ghandidev <ghandiaboultaif@gmail.com>
2024-05-03 16:18:07 -0400

    adding functions getMean and getMedian

--- script.js
+++ script.js
@@ -22,7 +22,18 @@ const getMedian = (array) => {
   } */
 };
 
-const getMode = (array) => {};
+const getMode = (array) => {
+  const counts = {};
+  array.forEach((el) => (counts[el] = (counts[el] || 0) + 1));
+  if (new Set(Object.values(counts)).size === 1) {
+    return null;
+  }
+  const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
+  const mode = Object.keys(counts).filter(
+    (el) => counts[el] === counts[highest]
+  );
+  return mode.join(', ');
+};
 
 const calculate = () => {
   const value = document.querySelector('#numbers').value;
@@ -31,8 +42,10 @@ const calculate = () => {
 
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
   document.querySelector('#mean').textContent = mean;
   document.querySelector('#median').textContent = median;
+  document.querySelector('#mode').textContent = mode;
 };
 
 /* const testArr1 = [1, 2, 3, 4, 5];
