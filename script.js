c9a5858179ff4fd3efe75ba6fe4c5f8272123636
ghandidev <ghandiaboultaif@gmail.com>
2024-05-03 17:41:35 -0400

    complete function getVariance

--- script.js
+++ script.js
@@ -31,12 +31,13 @@ const getRange = (array) => {
 
 const getVariance = (array) => {
   const mean = getMean(array);
-  const differences = array.map((el) => el - mean);
-  const squaredDifferences = differences.map((el) => el ** 2);
-  const sumSquaredDifferences = squaredDifferences.reduce(
-    (acc, el) => acc + el,
-    0
-  );
+  const variance =
+    array.reduce((acc, el) => {
+      const difference = el - mean;
+      const squared = difference ** 2;
+      return acc + squared;
+    }, 0) / array.length;
+  return variance;
 };
 
 const calculate = () => {
@@ -48,9 +49,11 @@ const calculate = () => {
   const median = getMedian(numbers);
   const mode = getMode(numbers);
   const range = getRange(numbers);
+  const variance = getVariance(numbers);
 
   document.querySelector('#mean').textContent = mean;
   document.querySelector('#median').textContent = median;
   document.querySelector('#mode').textContent = mode;
   document.querySelector('#range').textContent = range;
+  document.querySelector('#variance').textContent = variance;
 };
